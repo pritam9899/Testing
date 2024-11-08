508eb3d1c0202bb50a89e791517cf1158da5fad7
Pritam Singh <61503282+pritam9899@users.noreply.github.com>
2024-11-08 22:54:29 +0530

    thumbnail image url variable name changed

--- loadJsonData.js
+++ loadJsonData.js
@@ -33,9 +33,9 @@ function displayData(courseData) {
 
         // Add a thumbnail image
         const thumbnail_Id=extractFileId(course.thumbnail_id);
-        const thumbnail_img=loadDriveFile(thumbnail_Id);
+        const thumbnail_imgURL=loadDriveFile(thumbnail_Id);
         const thumbnail = document.createElement("img");
-        thumbnail.src = thumbnail_image;//"https://i0.wp.com/www.powerbi-influential.com/wp-content/uploads/2022/05/power-bi-april-features.png?res";
+        thumbnail.src = thumbnail_imgURL;//"https://i0.wp.com/www.powerbi-influential.com/wp-content/uploads/2022/05/power-bi-april-features.png?res";
         thumbnail.alt = "Course Thumbnail";
         thumbnail.className = "course-thumbnail";
         
